3b409eb466cf06ba416c2bf9b3c69f0a1d482643
Rahman Qureshi <rahman.a.qureshi@gmail.com>
2016-01-23 15:24:36 -0500

    test

--- code/app.py
+++ code/app.py
@@ -1,15 +1,23 @@
 from flask import Flask, request, redirect
 import twilio.twiml
 import os
+from WebMd import WebMd
+from TopicExtractor import TopicExtractor
+ 
  
 app = Flask(__name__)
+
+webmd = WebMd() 
+topic_extractor = TopicExtractor()
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
- 
+    incoming_message = ""
+    print request.form
     resp = twilio.twiml.Response()
-    resp.message("Hello, Mobile Monkey")
+    resp.message('Hello World')
+    #resp.message(webmd.search(topic_extractor.extract(incoming_message)))
     return str(resp)
  
 if __name__ == "__main__":
