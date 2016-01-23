31f82e465f12aad087d0c48f84accfc6a0462abc
Rahman Qureshi <rahman.a.qureshi@gmail.com>
2016-01-23 16:05:45 -0500

    successful scraping on heroku

---
+++ code/README.md
@@ -0,0 +1,3 @@
+## Usage
+
+1. Download ntlk data into code/nltk_data
--- code/app.py
+++ code/app.py
@@ -3,21 +3,19 @@ import twilio.twiml
 import os
 from WebMd import WebMd
 from TopicExtractor import TopicExtractor
- 
+import nltk
  
 app = Flask(__name__)
-
+nltk.data.path.append('./nltk_data/')
 webmd = WebMd() 
 topic_extractor = TopicExtractor()
  
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
     """Respond to incoming calls with a simple text message."""
-    incoming_message = ""
-    print request.form
+    incoming_message = request.form.get('Body', '')
     resp = twilio.twiml.Response()
-    resp.message('Hello World')
-    #resp.message(webmd.search(topic_extractor.extract(incoming_message)))
+    resp.message(webmd.search(topic_extractor.extract(incoming_message)))
     return str(resp)
  
 if __name__ == "__main__":
