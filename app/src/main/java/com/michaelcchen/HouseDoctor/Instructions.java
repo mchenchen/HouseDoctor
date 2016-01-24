9c4597592bcbd9e32bcd967dc73ecb90bfe9141b
Michael Chen <michaelc.chen@mail.utoronto.ca>
2016-01-23 19:53:14 -0500

    finished version without json

---
+++ app/src/main/java/com/michaelcchen/HouseDoctor/Instructions.java
@@ -0,0 +1,14 @@
+package com.michaelcchen.HouseDoctor;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Instructions extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instructions);
+    }
+
+}
